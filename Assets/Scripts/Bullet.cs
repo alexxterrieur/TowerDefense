dbaf75d41c86107dc3abd935d17d3a143dcb5b0d
andyDPY <a.dupouy@lahorde.school>
2024-01-09 14:39:25 +0100

    Feat-TurretShooting
    Can shoot enemy and destroy them

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+    {
+        //particles effect with enemy impact
+        GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/TurretShoot.cs
+++ Assets/Scripts/TurretShoot.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class TurretShoot : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
+
     public float range = 15f;
+    public float fireRate = 1f;
+    public float fireCountDown = 0f;
+
+    [Header("Parameter")]
 
     public Transform rotateTurret;
     public float turnSpeed = 10f;
     public string enemyTag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
 
     private void Start()
     {
@@ -52,6 +63,26 @@ public class TurretShoot : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(rotateTurret.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         rotateTurret.rotation = Quaternion.Euler( 0f, rotation.y, 0f);
+
+        //shoot if the is in the array
+        if(fireCountDown < 0f)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;  
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO  = (GameObject) Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()//can see the range of the turret
