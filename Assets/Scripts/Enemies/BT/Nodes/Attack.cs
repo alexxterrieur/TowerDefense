de00a7789b794dd656ad77d8b5e8f02ddac5f26e
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-01-10 16:18:27 +0100

    BT v1

--- Assets/Scripts/Enemies/BT/Nodes/Attack.cs
+++ Assets/Scripts/Enemies/BT/Nodes/Attack.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Attack : Node
 {
-    public Attack()
-    {
+    private EnemyAI _enemyAI;
 
+    public Attack(EnemyAI enemyAI)
+    {
+        _enemyAI = enemyAI;
     }
 
     public override NodeState Evaluate()
     {
+        _enemyAI.Attack();
+
         _nodeState = NodeState.SUCCESS;
         return _nodeState;
     }
--- Assets/Scripts/Enemies/BT/Nodes/RushTower.cs
+++ Assets/Scripts/Enemies/BT/Nodes/RushTower.cs
@@ -7,15 +7,18 @@ public class RushTower : Node
 {
     private GameObject _tower;
     private NavMeshAgent _agent;
+    private EnemyAI _enemyAI;
 
-    public RushTower(GameObject tower, NavMeshAgent agent)
+    public RushTower(GameObject tower, NavMeshAgent agent, EnemyAI enemyAI)
     {
         _tower = tower;
         _agent = agent;
+        _enemyAI = enemyAI;
     }
 
     public override NodeState Evaluate()
     {
+        //_enemyAI.target = _enemyAI.tower;
         _agent.SetDestination(_tower.transform.position);
 
         _nodeState = NodeState.SUCCESS;
