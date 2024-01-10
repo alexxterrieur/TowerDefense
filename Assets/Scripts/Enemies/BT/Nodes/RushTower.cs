a53f2eeed6ac1df62f2499a2fff157ccaebd42c5
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-01-10 09:47:04 +0100

    Mise en commun

--- Assets/Scripts/Enemies/BT/Nodes/RushTower.cs
+++ Assets/Scripts/Enemies/BT/Nodes/RushTower.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RushTower : Node
 {
-    public RushTower()
-    {
+    private GameObject _tower;
+    private NavMeshAgent _agent;
 
+    public RushTower(GameObject tower, NavMeshAgent agent)
+    {
+        _tower = tower;
+        _agent = agent;
     }
 
     public override NodeState Evaluate()
     {
+        _agent.SetDestination(_tower.transform.position);
+
         _nodeState = NodeState.SUCCESS;
         return _nodeState;
     }
--- Assets/Scripts/Enemies/BT/Nodes/RushTurret.cs
+++ Assets/Scripts/Enemies/BT/Nodes/RushTurret.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RushTurret : Node
 {
-    public RushTurret()
-    {
+    private GameObject _turret;
+    private NavMeshAgent _agent;
 
+    public RushTurret(GameObject turret, NavMeshAgent agent)
+    {
+        _turret = turret;
+        _agent = agent;
     }
 
     public override NodeState Evaluate()
     {
+        _agent.SetDestination(_turret.transform.position);
+
         _nodeState = NodeState.SUCCESS;
         return _nodeState;
     }
